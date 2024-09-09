b77a36d513630a73604fb5603f2a422c851d0408
Xinyu Zhou <zxytim@gmail.com>
2024-09-09 14:10:49 +0800

    add json2csv alias

--- .zsh/utils.zsh
+++ .zsh/utils.zsh
@@ -21,3 +21,5 @@ version_compare_less_equal() {
 version_compare_greater_equal() {
     [ "$(echo "$1\n$2" | sort -V | head -n 1)" = "$2" ]
 }
+
+alias json2csv="jq -r '(map(keys) | add | unique) as \$cols | map(. as \$row | \$cols | map(\$row[.])) as \$rows | \$cols, \$rows[] | @csv'"
